321653fd19715b560605404e971a3c03a170934e
Hyeonseong <76834414+KiKi-Daehaksaeng@users.noreply.github.com>
2023-02-27 22:52:40 +0900

    test

--- src/main/java/com/test/demo/Service/UserService.java
+++ src/main/java/com/test/demo/Service/UserService.java
@@ -3,24 +3,23 @@ package com.test.demo.Service;
 import com.test.demo.Model.User;
 import com.test.demo.dao.UserDao;
 import lombok.RequiredArgsConstructor;
-=
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class UserService implements UserDetailsService {
+public class UserService {
     private final UserDao userDao;
 
-    @Override
-    public UserDetails loadUserByUsername(String id) throws UsernameNotFoundException {
-
-        User user = userDao.findUser(id);
-
-        if( user == null ) {
-            throw new UsernameNotFoundException(id);
-        }
-        return user;
-    }
+//    @Override
+//    public UserDetails loadUserByUsername(String id) throws UsernameNotFoundException {
+//
+//        User user = userDao.findUser(id);
+//
+//        if( user == null ) {
+//            throw new UsernameNotFoundException(id);
+//        }
+//        return user;
+//    }
     public User findUser(String id){
         return userDao.findUser(id);
     }
